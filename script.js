daa9704a38f4a543a956c6c4904a558592c6f5f0
mateo ossa <mateo.ossa.b@gmail.com>
2021-02-27 03:38:06 -0300

    añadiendo reload

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
+var escuchar_teclado = document.addEventListener("keydown",reload);
+
+function reload(x){
+    if (x.keyCode==82){
+        leo();
+    }
+    
+}
+
 var boton = document.getElementById("button-real");
 var imagen = document.getElementById("imagen")
 
@@ -5,4 +15,8 @@ boton.addEventListener("click", botonOprimido);
 
 function botonOprimido(){
     imagen.src = "./imagenes/leo-out.jpg"
+}
+
+function leo(){
+    imagen.src="./imagenes/leo.png"
 }
